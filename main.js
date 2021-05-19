946b0f15f08b5821d0eb6474c1de98dac53e93c9
Skiiinnny <nataniel.soto.pulido@gmail.com>
2021-05-19 10:23:38 -0400

    Implementación jquery y btn clima

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var x = document.getElementById("geo");
+
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "La localización no se pudo conseguir.";
+        jQuery('#geo').html('La localización no se pudo conseguir.');
     }
 }
 
@@ -12,10 +12,20 @@ function showPosition(position) {
     var latitud = position.coords.latitude;
     var longitud = position.coords.longitude;
     var linkAPI = 'http://api.weatherapi.com/v1/current.json?key=261a3397a09c419cb67155250211405&q=';
-    var linkFinal = linkAPI + latitud + ',' +longitud;
+    var linkFinal = linkAPI + latitud + ',' + longitud;
     jQuery.getJSON(linkFinal,
-    function (data) {
-        x.innerHTML= data.location.name+ ', '+data.location.country+': '+ data.current.temp_c + ' °C';
-    });
+        function (data) {
+            // x.innerHTML= data.location.name+ ', '+data.location.country+': '+ data.current.temp_c + ' °C';
+            jQuery('#geo').html(data.location.name + ', ' + data.location.country + ': ' + data.current.temp_c + ' °C');
+
+        });
 
 }
+
+jQuery(document).ready(function () {
+    // getLocation();
+
+    jQuery('#btnClima').click(function(){
+        getLocation();
+    });
+});
