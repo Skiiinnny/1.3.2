df6012b6d2350204044d975d3e00f61b48fc7731
Skiiinnny <nataniel.soto.pulido@gmail.com>
2021-07-05 12:25:52 -0400

    update repository

--- TestDjango/core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Animal
-
-class AnimalForm(ModelForm):
-
-    class Meta:
-        model = Animal
-        fields = ['N°chip','Nombre','Edad','Sexo','Descripcion']
